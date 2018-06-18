a2a85bad3125c26cd5d0beeef4dbe259d0c0035a
Ilya Savchuk <ilyas.sporichev@gmail.com>
2018-06-18 14:30:31 +0300

    edit line logics implemented

--- src/js/event-listeners/editCurrentLine.js
+++ src/js/event-listeners/editCurrentLine.js
@@ -11,37 +11,6 @@ const {
 const editCurrentLine = (index, event) => {
 
     const id = event.target.getAttribute('data-for')
-    const editButton = document.querySelector(`#${id} .group-edit-button`)
-    const mainButton = document.querySelector(`#${id} .group-main-button`)
-    const mainSection = document.querySelector(`#${id} .group-main-section`)
-    const editSection = document.querySelector(`#${id} .group-edit-section`)
-
-    const editLineButtonCollection = document.querySelectorAll(`#${id} .edit-line-button`)
-
-    const currentEditButton = event.target
-    const currentTr = event.target.parentNode.parentNode
-    const currentTbody = document.querySelector(`[source-data="${id}"]`)
-
-    currentEditButton.classList.toggle('active-edit-button')
-
-    const activeEditButton = document.querySelector(`#${id} .active-edit-button`)
-
-    currentTbody.classList.toggle('editting-tbody')
-    currentTr.classList.toggle('editting-line')
-
-    Array.from(editLineButtonCollection).map(el => {
-        // if (el.classList.contains('active-edit-button')) console.log(el)
-        if (!activeEditButton) el.removeAttribute('disabled')
-        else if (!el.classList.contains('active-edit-button')) el.setAttribute('disabled', true)
-    })
-
-    currentEditButton.innerHTML === exitEditModeText ? currentEditButton.innerHTML = startEditModeText : currentEditButton.innerHTML = exitEditModeText
-
-    /*******************************************************************************************/
-
-    const addPanel = document.querySelector(`#${id} .group-add-panel`)
-
-    addPanel.classList.toggle('hide')
 
     /*******************************************************************************************/
 
@@ -65,13 +34,45 @@ const editCurrentLine = (index, event) => {
     sellPriceInputEditting.value = sellPriceInputCellCollection[index].innerText
     sizeInputEditting.value = sizeInputCellCollection[index].innerText
 
-    updateButton.addEventListener('click', () => {
+    const updateLine = () => {
         nameInputCellCollection[index].innerText = nameInputEditting.value
         colorInputCellCollection[index].innerText = colorInputEditting.value
         costPriceInputCellCollection[index].innerText = costPriceInputEditting.value
         sellPriceInputCellCollection[index].innerText = sellPriceInputEditting.value
         sizeInputCellCollection[index].innerText = sizeInputEditting.value
+    }
+
+    updateButton.addEventListener('click', updateLine, { once: true })
+
+    /****************************************************************************/
+
+    const editLineButtonCollection = document.querySelectorAll(`#${id} .edit-line-button`)
+
+    const currentEditButton = event.target
+    const currentTr = event.target.parentNode.parentNode
+    const currentTbody = document.querySelector(`[source-data="${id}"]`)
+
+    currentEditButton.classList.toggle('active-edit-button')
+
+    const activeEditButton = document.querySelector(`#${id} .active-edit-button`)
+
+    currentTbody.classList.toggle('editting-tbody')
+    currentTr.classList.toggle('editting-line')
+
+    Array.from(editLineButtonCollection).map(el => {
+        if (!activeEditButton) {
+            updateButton.removeEventListener('click', updateLine)
+            el.removeAttribute('disabled')
+        }
+        else if (!el.classList.contains('active-edit-button')) el.setAttribute('disabled', true)
     })
+
+    currentEditButton.innerHTML === exitEditModeText ? currentEditButton.innerHTML = startEditModeText : currentEditButton.innerHTML = exitEditModeText
+
+    /*******************************************************************************************/
+
+    const addPanel = document.querySelector(`#${id} .group-add-panel`)
+    addPanel.classList.toggle('hide')
 
 }
 
