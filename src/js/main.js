15952d78b79bcde085bde26ccbe3ba390d9a4172
Ilya Savchuk <ilyas.sporichev@gmail.com>
2018-09-19 19:30:48 +0300

    header component added, add new group logic added

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 import mdInit from 'js/mdInit'
 import Group from 'group'
+import Header from 'header'
 
 (function () {
+    new Header().create()
+
     const globalGroupCollection = JSON.parse(localStorage.getItem('countingCheck'))
-    // console.log(32321, globalGroupCollection)
     if (!globalGroupCollection) {
         localStorage.setItem('countingCheck', JSON.stringify({}))
     } else {
         Object.keys(globalGroupCollection).map(groupName => {
             const newGroupInstance = new Group(groupName)
             newGroupInstance.create()
-            console.log(newGroupInstance)
-            console.log(globalGroupCollection)
+            // console.log(newGroupInstance)
+            // console.log(globalGroupCollection)
 
             newGroupInstance.goodsMap = globalGroupCollection[groupName]
-
-            // console.log(323, newGroupInstance)
 
             const currentGoodCollection = Object.keys(globalGroupCollection[groupName])
             currentGoodCollection.map(positionId => {
@@ -36,8 +36,6 @@ import Group from 'group'
                     sellPrice,
                     positionId
                 )
-
-                console.log(222, newGroupInstance)
             })
 
             document.body.appendChild(newGroupInstance.groupDomModel)
